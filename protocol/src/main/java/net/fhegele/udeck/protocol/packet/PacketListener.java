87162ac7405f07c017de1f0df315887a50bdf2a8
FlorianHegele <144370530+FlorianHegele@users.noreply.github.com>
2024-09-01 11:49:56 +0200

    add protocol inside packet listener

--- protocol/src/main/java/net/fhegele/udeck/protocol/packet/PacketListener.java
+++ protocol/src/main/java/net/fhegele/udeck/protocol/packet/PacketListener.java
@@ -1,7 +1,11 @@
 package net.fhegele.udeck.protocol.packet;
 
+import net.fhegele.udeck.protocol.ConnectionProtocol;
+
 public interface PacketListener {
 
     PacketFlow getFlow();
+
+    ConnectionProtocol getProtocol();
 
 }
