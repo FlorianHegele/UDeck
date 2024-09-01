c0f64925ead2b2b6b5d49c2816ec01c2a75177a1
FlorianHegele <144370530+FlorianHegele@users.noreply.github.com>
2024-09-01 11:51:13 +0200

    update management of protocol key add function to handle packet and change the packet listener change setInitProtocolAttributes function to setProtocolAttributes function (more generic)

--- protocol/src/main/java/net/fhegele/udeck/protocol/Connection.java
+++ protocol/src/main/java/net/fhegele/udeck/protocol/Connection.java
@@ -3,17 +3,14 @@ package net.fhegele.udeck.protocol;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.util.AttributeKey;
 import net.fhegele.udeck.protocol.packet.Packet;
 import net.fhegele.udeck.protocol.packet.PacketFlow;
 import net.fhegele.udeck.protocol.packet.PacketListener;
+import org.jetbrains.annotations.NotNull;
 
 import java.net.SocketAddress;
 
 public class Connection extends SimpleChannelInboundHandler<Packet<?>> {
-
-    public static final AttributeKey<ConnectionProtocol.CodecData<?>> ATTRIBUTE_SERVER_RECEIVE_PROTOCOL = PacketFlow.SERVER_RECEIVE.getAttributeKey();
-    public static final AttributeKey<ConnectionProtocol.CodecData<?>> ATTRIBUTE_CLIENT_RECEIVE_PROTOCOL = PacketFlow.CLIENT_RECEIVE.getAttributeKey();
 
     private final PacketFlow receiveFlow;
     private PacketListener packetListener;
@@ -37,12 +34,36 @@ public class Connection extends SimpleChannelInboundHandler<Packet<?>> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Packet<?> packet) {
+        if(packetListener == null) throw new IllegalStateException("Received a packet before the initialization of the packet listener");
 
+        try {
+            handlePacket(packet, packetListener);
+        } catch (ClassCastException castException) {
+            throw new IllegalStateException("Packet (" + packet.getClass().getSimpleName() + ") handled on a wrong packet listener (" + packetListener.getClass().getSimpleName() + ")");
+        }
     }
 
-    public static void setInitProtocolAttributes(Channel channel) {
-        channel.attr(ATTRIBUTE_SERVER_RECEIVE_PROTOCOL).set(ConnectionProtocol.INIT_CONNECTION.getCodecData(PacketFlow.SERVER_RECEIVE));
-        channel.attr(ATTRIBUTE_CLIENT_RECEIVE_PROTOCOL).set(ConnectionProtocol.INIT_CONNECTION.getCodecData(PacketFlow.CLIENT_RECEIVE));
+    @SuppressWarnings("unchecked")
+    private static <T extends PacketListener> void handlePacket(Packet<T> packet, @NotNull PacketListener packetListener) {
+        packet.handle((T) packetListener);
+    }
+
+    public void setPacketListener(PacketListener packetListener) {
+        if(packetListener == null) throw new IllegalArgumentException("packetListener must not be null");
+
+        final PacketFlow flow = packetListener.getFlow();
+        if(flow != receiveFlow) throw new IllegalArgumentException("Trying to set a packet listener with opposite flow, the current flow is " + receiveFlow + " but you are trying to set the flow " + flow);
+
+        final ConnectionProtocol protocol = packetListener.getProtocol();
+        final ConnectionProtocol usedProtocol = channel.attr(flow.getProtocolKey()).get().getProtocol();
+        if(usedProtocol != protocol) throw new IllegalArgumentException("Trying to set a packet listener with other protocol, the current protocol is " + usedProtocol.name() + " but you are trying to set the protocol " + protocol.name());
+
+        this.packetListener = packetListener;
+    }
+
+    public static void setProtocolAttributes(Channel channel, ConnectionProtocol protocol) {
+        channel.attr(PacketFlow.SERVER_RECEIVE.getProtocolKey()).set(protocol.getCodecData(PacketFlow.SERVER_RECEIVE));
+        channel.attr(PacketFlow.CLIENT_RECEIVE.getProtocolKey()).set(protocol.getCodecData(PacketFlow.CLIENT_RECEIVE));
     }
 
     public boolean isConnected() {
--- protocol/src/main/java/net/fhegele/udeck/protocol/packet/PacketFlow.java
+++ protocol/src/main/java/net/fhegele/udeck/protocol/packet/PacketFlow.java
@@ -7,11 +7,18 @@ public enum PacketFlow {
 
     CLIENT_RECEIVE, SERVER_RECEIVE;
 
+    private final AttributeKey<ConnectionProtocol.CodecData<?>> attributeKey;
+
+    PacketFlow() {
+        this.attributeKey = AttributeKey.valueOf(name().toLowerCase());
+    }
+
     public PacketFlow getOpposite() {
         return this == CLIENT_RECEIVE ? SERVER_RECEIVE : CLIENT_RECEIVE;
     }
 
-    public AttributeKey<ConnectionProtocol.CodecData<?>> getAttributeKey() {
-        return AttributeKey.valueOf(name().toLowerCase());
+    public AttributeKey<ConnectionProtocol.CodecData<?>> getProtocolKey() {
+        return attributeKey;
     }
+
 }
