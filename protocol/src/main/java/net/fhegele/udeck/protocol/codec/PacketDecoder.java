cdec63ae7ce38a1899eee140728b8837afd83646
FlorianHegele <144370530+FlorianHegele@users.noreply.github.com>
2024-09-01 12:03:50 +0200

    update PacketEncoder and PacketDecoder using protocolKey

--- protocol/src/main/java/net/fhegele/udeck/protocol/codec/PacketDecoder.java
+++ protocol/src/main/java/net/fhegele/udeck/protocol/codec/PacketDecoder.java
@@ -3,17 +3,26 @@ package net.fhegele.udeck.protocol.codec;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.util.AttributeKey;
+import net.fhegele.udeck.protocol.ConnectionProtocol;
 import net.fhegele.udeck.protocol.packet.BadPacketException;
 import net.fhegele.udeck.protocol.packet.Packet;
+import net.fhegele.udeck.protocol.packet.PacketFlow;
 
 import java.util.List;
 
 public class PacketDecoder extends ByteToMessageDecoder {
 
+    private final AttributeKey<ConnectionProtocol.CodecData<?>> protocolKey;
+
+    public PacketDecoder(PacketFlow receivedFlow) {
+        this.protocolKey = receivedFlow.getProtocolKey();
+    }
+
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
         final int packetId = in.readUnsignedShort();
-        final Packet<?> packet = null;
+        final Packet<?> packet = ctx.channel().attr(protocolKey).get().createPacket(packetId, in);
 
         if(packet == null) throw new BadPacketException("Packet with ID" + packetId + " doesn't exist");
 
--- protocol/src/main/java/net/fhegele/udeck/protocol/codec/PacketEncoder.java
+++ protocol/src/main/java/net/fhegele/udeck/protocol/codec/PacketEncoder.java
@@ -3,14 +3,23 @@ package net.fhegele.udeck.protocol.codec;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
+import io.netty.util.AttributeKey;
+import net.fhegele.udeck.protocol.ConnectionProtocol;
 import net.fhegele.udeck.protocol.packet.Packet;
+import net.fhegele.udeck.protocol.packet.PacketFlow;
 
 public class PacketEncoder extends MessageToByteEncoder<Packet<?>> {
 
+    private final AttributeKey<ConnectionProtocol.CodecData<?>> protocolKey;
+
+    public PacketEncoder(PacketFlow receivedFlow) {
+        this.protocolKey = receivedFlow.getProtocolKey();
+    }
+
     @Override
     protected void encode(ChannelHandlerContext ctx, Packet<?> packet, ByteBuf out) {
-        final int id = 0;
-        out.writeShort(id); // 2 bytes
+        final int packetId = ctx.channel().attr(protocolKey).get().packetId(packet);;
+        out.writeShort(packetId); // 2 bytes
         packet.write(out);
     }
 }
