640de8e22ddc0c40b4d19252c8e50d525bf67c6c
FlorianHegele <144370530+FlorianHegele@users.noreply.github.com>
2024-09-02 23:00:12 +0200

    add default client code to communicate with the server

---
+++ client/src/main/java/net/fhegele/udeck/client/Client.java
@@ -0,0 +1,67 @@
+package net.fhegele.udeck.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import net.fhegele.udeck.client.protocol.ClientPacketListenerImpl;
+import net.fhegele.udeck.protocol.Connection;
+import net.fhegele.udeck.protocol.ConnectionProtocol;
+import net.fhegele.udeck.protocol.packet.PacketFlow;
+
+import java.io.IOException;
+
+public class Client {
+
+    private Connection connection;
+
+    private final int port;
+    private final String host;
+
+    public Client(int port, String host) {
+        this.port = port;
+        this.host = host;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final Client client = new Client(28868, "localhost");
+
+        // trying to connect client to the server
+        client.connect();
+
+        System.out.println("end of program");
+    }
+
+    public void connect() throws InterruptedException {
+        try (final NioEventLoopGroup group = new NioEventLoopGroup()) {
+            new Bootstrap().group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) {
+                            final ChannelPipeline pipeline = socketChannel.pipeline();
+
+                            final PacketFlow flow = PacketFlow.CLIENT_BOUND;
+                            Connection.configureCodec(pipeline, flow);
+                            Connection.setProtocolAttributes(pipeline.channel(), ConnectionProtocol.INIT);
+
+                            connection = new Connection(flow);
+                            pipeline.addLast(connection);
+                        }
+                    })
+                    .connect(host, port).sync();
+
+            final Keyboard keyboard = new Keyboard(connection);
+            connection.setPacketListener(new ClientPacketListenerImpl());
+
+            try {
+                keyboard.handle();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
---
+++ client/src/main/java/net/fhegele/udeck/client/Keyboard.java
@@ -0,0 +1,58 @@
+package net.fhegele.udeck.client;
+
+import net.fhegele.udeck.protocol.Connection;
+import net.fhegele.udeck.protocol.packet.Packet;
+import net.fhegele.udeck.protocol.packet.init.PingPacket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Keyboard {
+
+    private final Connection connection;
+    private BufferedReader in;
+
+    private boolean isReading;
+
+    public Keyboard(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void handle() throws IOException {
+        if(isReading) throw new IllegalStateException("Keyboard is already reading.");
+
+        in = new BufferedReader(new InputStreamReader(System.in));
+        isReading = true;
+
+        while (isReading) {
+            printAction();
+
+            final String message = in.readLine();
+
+            if(message == null || message.isBlank()) {
+                stop();
+                return;
+            }
+
+            final Packet<?> packet;
+            switch (message) {
+                case "1" -> packet = new PingPacket();
+                default -> packet = null;
+            }
+
+            if(packet != null) connection.sendPacket(packet);
+        }
+    }
+
+    private void printAction() {
+        System.out.println("empty -> quit");
+        System.out.println("1 -> ping packet");
+        System.out.println("anything else -> do nothing");
+    }
+
+    public void stop() {
+        if(!isReading) throw new IllegalStateException("Keyboard is not reading.");
+        isReading = false;
+    }
+}
